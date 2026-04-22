c88adc0c04b96e7ffea4ca62007f6a5d9a4f64a4
unknown <anoreen645@gmail.com>
2026-04-22 23:37:41 +0500

    added signup/login pages

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+/*import React from 'react';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;*/
+import React from "react";
+import Signup from "./Pages/signup";
+
+function App() {
+  return(
+   <div className="App">
+    <Signup/>
+   </div>
+  );
+
+  
+}
+export default App;
